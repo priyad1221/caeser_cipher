5f79b7984de9d0ab11e3ea850b261a1ce6d4aadd
BANU PRIYA D <139879880+priyad1221@users.noreply.github.com>
2023-09-10 22:19:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ print("CAESER CIPHER ")
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
          'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-direction = input("Type encode or decode: \n ")
-text = input("Enter a text: \n")
-shift = int(input("Enter a shift number: \n "))
+
 
 def e_display(e_text,e_shift):
     encode = " "
@@ -24,8 +22,20 @@ def d_display(d_text,d_shift):
         decode+= new_letter
     print(f"The decoded text is: {decode}")
 
+repition = True
+while repition:
+         direction = input("Type encode or decode: \n ")
+         text = input("Enter a text: \n")
+         shift = int(input("Enter a shift number: \n "))
+         shift_mod = shift%26
 
-if direction == "encode":
-    e_display(e_text=text, e_shift=shift)
-elif direction == "decode":
-    d_display(d_text=text, d_shift=shift)
+
+         if direction == "encode":
+                  e_display(e_text=text, e_shift=shift)
+         elif direction == "decode":
+                  d_display(d_text=text, d_shift=shift)
+
+         should_continue = input("Do u want to continue Type yes or no :/n")
+         if should_continue==no:
+                  repition = False
+                  print("Bye")
